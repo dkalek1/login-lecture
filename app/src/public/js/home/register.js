58d5ef561c83e5455f09420801c28f8c016359d0
dkalek1 <jiwang0917@naver.com>
2022-11-22 14:36:14 +0900

    회원가입 라우터 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,11 +9,14 @@ const id = document.querySelector('#id'),
 registerBtn.addEventListener('click', register);
 
 function register() {
+  if (!id.value) return alert('아이디를 입력해주세요');
+  if (psword.value !== confirmPsword.value)
+    return alert('비번이 일치하지 않습니다');
+
   const req = {
     id: id.value,
     psword: psword.value,
     name: name.value,
-    confirmPsword: confirmPsword.value,
   };
 
   fetch('/register', {
